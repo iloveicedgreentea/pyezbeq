129d9f083c0f8dd1987305ae60b403eeeb757363
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2024-10-01 17:42:46 -0400

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyezbeq",
-    version="0.0.7",
+    version="0.0.8",
     author="iloveicedgreentea2",
     description="A package to control ezbeq",
     long_description=long_description,
